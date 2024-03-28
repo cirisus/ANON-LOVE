633649d012b46a0f9ede57cd5d49bea56aaae1e3
cirisus <cirisus@outlook.com>
2024-03-28 19:35:21 +0800

    ```

--- .history/anon-love/scripts/sync_20240328185000.js
+++ .history/anon-love/scripts/sync_20240328185000.js
@@ -1,12 +1,13 @@
 import { readFile, writeFile } from 'fs';
 
-const sourceFile = './index.html';
-const targetFile = './anon-love/src/lib/About/index.html';
+const sourceFile = 'index.html';
+const targetFile = 'src/libAbout/index.html';
 
 readFile(sourceFile, 'utf8', (err, data) => {
     if (err) {
         console.error(`Error reading file from disk: ${err}`);
     } else {
+
         writeFile(targetFile, data, (err) => {
             if (err) {
                 console.error(`Error writing file: ${err}`);
