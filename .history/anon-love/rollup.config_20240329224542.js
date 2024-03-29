690b3bb39bb006caf420167350154fc7c56d6806
cirisus <cirisus@outlook.com>
2024-03-29 22:50:18 +0800

    Update ``` - Navigator - global.css ```

--- .history/anon-love/rollup.config_20240329224542.js
+++ .history/anon-love/rollup.config_20240329224542.js
@@ -6,7 +6,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import livereload from 'rollup-plugin-livereload';
 import css from 'rollup-plugin-css-only';
 import sveltePreprocess from 'svelte-preprocess';
-import autoprefixer from 'autoprefixer';
 
 const production = !process.env.ROLLUP_WATCH;
 const preprocessor = sveltePreprocess({
@@ -14,7 +13,7 @@ const preprocessor = sveltePreprocess({
 	  includePaths: ['src'],
 	},
 	postcss: {
-	  plugins: [autoprefixer],
+	  plugins: [require('autoprefixer')],
 	},
   });
 
@@ -49,7 +48,7 @@ export default {
 	},
 	plugins: [
 		svelte({
-			preprocess: preprocessor,
+			preprocess: preprocess(),
 			compilerOptions: {
 				dev: !production
 			}
