31e3fe744756f3a8244fdf97268abe735e538ec6
cirisus <cirisus@outlook.com>
2024-04-06 16:55:27 +0800

    markdown

--- .history/anon-love/src/lib/layout_20240406165111.svelte
+++ .history/anon-love/src/lib/layout_20240406165111.svelte
@@ -1,16 +1,15 @@
 <script>
 	import { onMount } from 'svelte';
-	import SvelteMarkdown from 'svelte-markdown';
+	import { marked } from 'marked';
 	import Modal from './Components/Popup/+page.svelte';
 	import markdown from '../../../docs/RESOURCES.md';
 
+	let content = '';
 	let title = ' CONTENT USAGE SPECIFICATIONS';
 	let buttonText = `I'VE READ IT`;
 
-	let content;
-
 	onMount(() => {
-		content = markdown;
+		content = marked(markdown);
 	});
 </script>
 
@@ -25,7 +24,5 @@
 
 <footer>
 	<span class="footer">©2024 ANON-LOVE|<button on:click={Modal.toggleModal}>CONTENT USAGE SPECIFICATIONS</button></span>
-	<Modal {title} {buttonText}>
-		<SvelteMarkdown source={content} />
-	</Modal>
+	<Modal {content} {title} {buttonText} />
 </footer>
