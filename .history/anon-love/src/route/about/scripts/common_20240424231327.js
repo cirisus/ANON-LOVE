03c4e655290bf703bdb46febc9b335f0181e39d3
cirisus <cirisus@outlook.com>
2024-04-24 23:13:29 +0800

    Abort

---
+++ .history/anon-love/src/route/about/scripts/common_20240424231327.js
@@ -0,0 +1,77 @@
+let initialBlur = 150;
+let initialScale = 1.7;
+let blurDuration = 1250;
+let scaleDuration = 1305;
+
+function setInitialStyles(sibling) {
+    sibling.style.filter = `blur(${initialBlur}px)`;
+    sibling.style.transform = `scale(${initialScale})`;
+}
+
+function removeInitialStyles(sibling) {
+    sibling.style.filter = '';
+    sibling.style.transform = '';
+}
+
+function applyTransition(sibling) {
+    let blurAnimation = sibling.animate(
+        [
+            { filter: `blur(${initialBlur}px)`, offset: 0 },
+            { filter: 'blur(0px)', offset: 1 }
+        ],
+        {
+            duration: blurDuration,
+            easing: 'cubic-bezier(0.71, 0.21, 0.75, 0.97)'
+        }
+    );
+
+    let scaleAnimation = sibling.animate(
+        [
+            { transform: `scale(${initialScale})`, offset: 0 },
+            { transform: 'scale(1)', offset: 1 }
+        ],
+        {
+            duration: scaleDuration,
+            easing: 'cubic-bezier(0.71, 0.21, 0.75, 0.97)'
+        }
+    );
+
+    return [blurAnimation, scaleAnimation];
+}
+
+function blurAndScaleSiblings(loader) {
+    let sibling = loader.parentNode.firstChild;
+    while (sibling) {
+        if (sibling.nodeType === 1 && sibling !== loader) {
+            setInitialStyles(sibling);
+        }
+        sibling = sibling.nextSibling;
+    }
+}
+
+export function blurSiblingsOfLoader() {
+    const loader = document.querySelector('#loader');
+
+    if (loader) {
+        blurAndScaleSiblings(loader);
+
+        const handleEvent = () => {
+            let sibling = loader.parentNode.firstChild;
+            while (sibling) {
+                if (sibling.nodeType === 1 && sibling !== loader) {
+                    removeInitialStyles(sibling);
+                    applyTransition(sibling);
+                }
+                sibling = sibling.nextSibling;
+            }
+            ['click', 'touchstart', 'keydown'].forEach(event => {
+                document.removeEventListener(event, handleEvent);
+            });
+        };
+        setTimeout(() => {
+            ['click', 'touchstart', 'keydown'].forEach(event => {
+                document.addEventListener(event, handleEvent);
+            });
+        }, 3500);
+    }
+}
---
+++ .history/anon-love/src/route/index_20240424231327.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Router from 'svelte-spa-router'
+	import Layout from "./lib/layout.svelte";
+	import Home from "./lib/Home/+page.svelte";
+	import About from "./lib/About/+page.svelte";
+	import Test from "./lib/Test/+page.svelte";
+	export let name = 'ANON';
+
+	let routes = {
+		'/': Home,
+		'/about': About,
+		'/test': Test,
+		'*': Home
+	}
+</script>
+
+<Layout>
+	<Router {routes} let:component={Component}>
+		<svelte:component this={Component} {name} />
+	</Router>
+</Layout>
+
+<style lang="scss">
+	@import './app.scss';
+</style>
